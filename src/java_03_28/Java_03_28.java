a6f54420a54b5d687ee0bbcb41df1187f3f22ab0
richardvlucena <richard.v.lucena@protonmail.com>
2022-04-05 06:54:24 -0300

    first commit

---
+++ src/java_03_28/Java_03_28.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_03_28;
+
+/**
+ *
+ * @author aluno.etec
+ */
+public class Java_03_28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
